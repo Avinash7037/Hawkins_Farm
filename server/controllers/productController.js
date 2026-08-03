c5829e4a3163851da3782b56643e8a263aa9ed42
Avinash kumar <jhaclsxn8126@gmail.com>
2026-08-04 02:17:26 +0530

    added pagination and product listing api

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -38,6 +38,93 @@ const createProduct = async (req, res) => {
   }
 };
 
+const getProducts = async (req, res) => {
+  try {
+    const {
+      search,
+      category,
+      location,
+      minPrice,
+      maxPrice,
+      sort,
+      page = 1,
+      limit = 10,
+    } = req.query;
+
+    let query = {};
+
+    // Search
+    if (search) {
+      query.name = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    // Category
+    if (category) {
+      query.category = category;
+    }
+
+    // Location
+    if (location) {
+      query.location = location;
+    }
+
+    // Price
+    if (minPrice || maxPrice) {
+      query.price = {};
+
+      if (minPrice) query.price.$gte = Number(minPrice);
+      if (maxPrice) query.price.$lte = Number(maxPrice);
+    }
+
+    // Sorting
+    let sortOption = { createdAt: -1 };
+
+    if (sort === "oldest") {
+      sortOption = { createdAt: 1 };
+    }
+
+    if (sort === "priceLow") {
+      sortOption = { price: 1 };
+    }
+
+    if (sort === "priceHigh") {
+      sortOption = { price: -1 };
+    }
+
+    // Pagination
+    const currentPage = Number(page);
+    const pageSize = Number(limit);
+
+    const skip = (currentPage - 1) * pageSize;
+
+    const totalProducts = await Product.countDocuments(query);
+
+    const products = await Product.find(query)
+      .populate("farmer", "name email")
+      .sort(sortOption)
+      .skip(skip)
+      .limit(pageSize);
+
+    res.status(200).json({
+      success: true,
+      currentPage,
+      totalPages: Math.ceil(totalProducts / pageSize),
+      totalProducts,
+      count: products.length,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createProduct,
+  getProducts,
 };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createProduct } = require("../controllers/productController");
-const { protect, authorize } = require("../middleware/authMiddleware");
+const {
+  createProduct,
+  getProducts,
+} = require("../controllers/productController");
 
+const { protect, authorize } = require("../middleware/authMiddleware");
+router.get("/", getProducts);
 router.post("/", protect, authorize("farmer"), createProduct);
 
 module.exports = router;
